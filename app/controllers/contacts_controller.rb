6e689ace9fdc6bb6ceff2a0900c32aa29add47e9
Stefan Sarmis <stefan.sarmis@googlemail.com>
2017-03-07 21:46:26 +0000

    Unused commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,9 +10,9 @@ class ContactsController < ApplicationController
          name  = params[:contact][:name]
          email = params[:contact][:email]
          body = params[:contact][:comments]
-         ContactMailer.contact_email(name, email, body).deliver
-         flash[:success] = "Message sent."
-         redirect_to new_contact_path
+         ContactMailer.contact_email(name, email, body).deliver # Sent Email
+         flash[:success] = "Message sent." # Flash Success Message
+         redirect_to new_contact_path # redirect to blank contact form
       else 
          flash[:danger] = @contact.errors.full_messages.join(", ")
          redirect_to new_contact_path
