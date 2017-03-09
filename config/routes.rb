af6e64cba94dc6cc5914d2417a72e5fbec61a090
Stefan Sarmis <stefan.sarmis@googlemail.com>
2017-03-09 21:28:32 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
     root to: 'pages#home'    
     get 'about', to: 'pages#about'
+    resources :users do
+      resource :profile
+    end
     resources :contacts, only: :create 
     get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
