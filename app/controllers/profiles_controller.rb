3ccd679765222f1352a691fa2fefd2560cf51289
Stefan Sarmis <stefan.sarmis@googlemail.com>
2017-03-10 21:56:26 +0000

    Added file field for image upload to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -23,6 +23,6 @@ class ProfilesController < ApplicationController
     #Whitelisting so no one can modify our Profile-Form
     private 
         def profile_params
-            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+            params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
         end
 end
