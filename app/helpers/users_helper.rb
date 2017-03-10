bb953715e4ac1f8c4a2564ae9f868908f4d61767
Stefan Sarmis <stefan.sarmis@googlemail.com>
2017-03-10 22:45:01 +0000

    Added helper for job title icon

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,16 @@
+module UsersHelper
+    def job_title_icon
+        if @user.profile.job_title == "Entwickler"
+            "<i class='fa fa-code'></i>".html_safe
+        elsif @user.profile.job_title == "Student"
+            "<i class='fa fa-weixin'>".html_safe
+        elsif @user.profile.job_title == "Animiations-Spezialist"
+            "<i class='fa fa-camera'></i>".html_safe
+        elsif @user.profile.job_title == "Autofahrer"
+            "<i class='fa fa-car'></i>".html_safe
+        elsif @user.profile.job_title == "Radiergummi"
+            "<i class='fa fa-eraser'></i>".html_safe
+        end
+        
+    end
+end
