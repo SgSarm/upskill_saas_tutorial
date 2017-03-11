f319f63548b945e4ef11d3fe441818cf03897eae
Stefan Sarmis <stefan.sarmis@googlemail.com>
2017-03-11 15:35:54 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,6 +20,12 @@ class ProfilesController < ApplicationController
             render action: :new
         end
     end
+    
+    # GET /users/:user_id/profile/edit(.:format)
+    def edit
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+    end
     #Whitelisting so no one can modify our Profile-Form
     private 
         def profile_params
